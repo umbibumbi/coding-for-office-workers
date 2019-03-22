0948d51d30c979e1d8791e99283c279cffa375ef
umbibumbi <umbibumbi88@gmail.com>
2019-03-22 21:17:03 +0700

    Add data-type-1.py

---
+++ conversation.py
@@ -0,0 +1,8 @@
+# : type # for comment-out or control /
+print(1 + 2)
+
+# 사용자로부터 값 받기
+# 변수 활용
+# name = input("당신의 이름은 무엇")
+# print("안녕하세요, " + name )
+print("안녕" + "하세요")
---
+++ data-type-1.py
@@ -0,0 +1,13 @@
+# 정수 int - integer
+print(1 + 1)
+# 소수 float
+print(1.5 + 2.1)
+# 문자 str - string
+print("Hello")
+
+#print("hello" + "1")
+print(int(4 / 2)) #소수를 정수로 표현하기
+print(float(2)) #정수를 소수로 표현하기
+
+print("1")
+print(int("1.5")) #error
